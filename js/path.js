54ef648eb92d4ed5886ad6b6d4528487ce3bd001
wufff <wfqjnzb@sina.com>
2019-04-12 14:46:49 +0800

    first

---
+++ js/path.js
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-11-02 14:36:21
+ * @version $Id$
+ */
+ 
+require(["config"],function(){
+
+})
---
+++ js/re-config.js
@@ -0,0 +1,14 @@
+require.config({
+	baseUrl: "/mySpace/js",
+	paths: {
+		"jquery":["http://apps.bdimg.com/libs/jquery/1.9.1/jquery.min"],
+		"expression":["ui/expression"],
+		"album":["ui/album"],
+		"path":["tools/path"],
+		"page":["tools/pags"],
+		"dialog":["ui/dialog"]
+	},
+	shim: {
+        
+	}
+});
